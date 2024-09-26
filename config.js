566af64ed67bcbffa8fb27d287178b770e62719f
nimsararesandu62 <nimsararesandu62@gmail.com>
2024-09-26 09:23:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "mongodb://mongo:UIvudqDxBwqJWjYaoueidhgdPPCFxjCO@junction.proxy.rlwy.net:39576",
 MONGODB: process.env.MONGODB || "",
 };
